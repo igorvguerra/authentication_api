5ede59c5ff8b3b5339a72b33c8ef39b69efd89df
Igor <igor.vguerra@hotmail.com>
2025-07-08 14:24:10 +0100

    create_user route created

--- app.py
+++ app.py
@@ -38,6 +38,20 @@ def logout():
     logout_user()
     return jsonify({"message": "User sucessfully Logout."})
 
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Registration successful."})
+
+    return jsonify({"message": "Invalid data."}), 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
